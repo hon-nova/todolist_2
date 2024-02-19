fda85ad405fcb7fe31d64d950c758204deeb7ba4
Hon Nguyen <hon.tnguyen294@gmail.com>
2024-02-19 02:09:20 -0800

    fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base:"/todolist_2/",
   plugins: [react()],
+  build: {
+    // Set custom HTML attributes for the output HTML files
+    html: {
+      // Add attributes to the script tag in the output HTML files
+      script: {
+        // Set the type attribute for JavaScript files to "module"
+        type: 'module',
+      },
+    }
+  }
 })
